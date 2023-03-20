d24a2a0c6bb99b389a7cbfe13676fadf8f201a72
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-03-20 09:07:23 -0300

    grey color on habits buttons

--- src/components/ContentHabits/ContentHabits.js
+++ src/components/ContentHabits/ContentHabits.js
@@ -8,28 +8,52 @@ export default function ContentHabitsAdd({ listHabits, deleteHabit }) {
         <Habit key={h.id} data-test="habit-container">
           <Trash>
             <h4 data-test="habit-name">{h.name}</h4>
-            <BsTrash onClick={() => deleteHabit(h.id)} data-test="habit-delete-btn" />
+            <BsTrash
+              onClick={() => deleteHabit(h.id)}
+              data-test="habit-delete-btn"
+            />
           </Trash>
           <WeekButtons>
-            <button className="buttons" data-test="habit-day">
+            <button
+              className={h.days.includes(0) ? "gray buttons" : "buttons"}
+              data-test="habit-day"
+            >
               D
             </button>
-            <button className="buttons" data-test="habit-day">
+            <button
+              className={h.days.includes(1) ? "gray buttons" : "buttons"}
+              data-test="habit-day"
+            >
               S
             </button>
-            <button className="buttons" data-test="habit-day">
+            <button
+              className={h.days.includes(2) ? "gray buttons" : "buttons"}
+              data-test="habit-day"
+            >
               T
             </button>
-            <button className="buttons" data-test="habit-day">
+            <button
+              className={h.days.includes(3) ? "gray buttons" : "buttons"}
+              data-test="habit-day"
+            >
               Q
             </button>
-            <button className="buttons" data-test="habit-day">
+            <button
+              className={h.days.includes(4) ? "gray buttons" : "buttons"}
+              data-test="habit-day"
+            >
               Q
             </button>
-            <button className="buttons" data-test="habit-day">
+            <button
+              className={h.days.includes(5) ? "gray buttons" : "buttons"}
+              data-test="habit-day"
+            >
               S
             </button>
-            <button className="buttons" data-test="habit-day">
+            <button
+              className={h.days.includes(6)  ? "gray buttons" : "buttons"}
+              data-test="habit-day"
+            >
               S
             </button>
           </WeekButtons>
